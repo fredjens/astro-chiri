c9643f8e2d38ca5ea8db4b207b1acd2389607610
Fredrik Jensen <post@fredrikjensen.com>
2025-10-18 20:55:38 +0200

    add start script

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
+    website: 'http://astro-chiri-9t0ci-36061322.localhost:8090/', // Site domain
     title: 'CHIRI', // Site title
     author: '3ASH', // Author name
     description: 'Minimal blog built by Astro', // Site description
